f16f247f6c453766b312b067327e1200be85c7a4
Ciaran Manca <ciaranmanca@gmail.com>
2021-04-02 12:47:38 +0200

    documented Student.java

--- src/Student.java
+++ src/Student.java
@@ -1,4 +1,9 @@
 class Student implements Comparable<Student> {
+  /*
+    Student class to create student objects that hold student ID, first name and last name.
+    Implents Comparable to compare two student objects.
+  */
+
   String studentNumber;
   String firstName;
   String lastName;
@@ -14,18 +19,22 @@ class Student implements Comparable<Student> {
   }
 
   public String getFirstName () {
+    // returns fist name
     return firstName;
   }
 
   public String getLastName () {
+    // returns last name
     return lastName;
   }
 
   public String toString() {
+    // returns studen ID, first name and last name in a string
     return studentNumber + " " + firstName + " " + lastName;
   }
 
   public int compareTo ( Student other) {
+    // compares this student object with another student object based on student ID
     return studentNumber.compareTo(other.studentNumber);
   }
 
